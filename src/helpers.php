6badfe780951c371ed2e74c9e919fde3364daced
Tim <git@nztm.net>
2019-05-22 08:24:23 +1200

    Explicit signature

--- src/helpers.php
+++ src/helpers.php
@@ -3,22 +3,22 @@
 use \NZTim\Logger\Logger;
 
 if (!function_exists('log_info')) {
-    function log_info(...$args): void
+    function log_info(string $channel, string $message, array $context = []): void
     {
-        app(Logger::class)->info(...$args);
+        app(Logger::class)->info($channel, $message, $context);
     }
 }
 
 if (!function_exists('log_warning')) {
-    function log_warning(...$args): void
+    function log_warning(string $channel, string $message, array $context = []): void
     {
-        app(Logger::class)->warning(...$args);
+        app(Logger::class)->warning($channel, $message, $context);
     }
 }
 
 if (!function_exists('log_error')) {
-    function log_error(...$args): void
+    function log_error(string $channel, string $message, array $context = []): void
     {
-        app(Logger::class)->error(...$args);
+        app(Logger::class)->error($channel, $message, $context);
     }
 }
